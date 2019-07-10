7fd41d9b2b23991cd2b4f0d3acc9705917f9be2a
deivisonmaster <deivisonoliveira.info@gmail.com>
2019-07-10 16:54:16 -0400

    Usando SOAP no servidor de aplicação

---
+++ src/br/com/caelum/estoque/ws/EstoqueWSImpl.java
@@ -0,0 +1,42 @@
+package br.com.caelum.estoque.ws;
+
+import java.util.Arrays;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "br.com.caelum.estoque.ws.EstoqueWS", serviceName = "EstoqueWS")
+public class EstoqueWSImpl implements EstoqueWS{
+
+	@Override
+	public ListaItens todosOsItens(Filtros filtros) {
+		System.out.println("Chamando todos os itens");
+		
+		ListaItens listaItens = new ListaItens();
+		listaItens.item = Arrays.asList(geraItem());
+		
+		return listaItens;
+	}
+
+	@Override
+	public CadastrarItemResponse cadastrarItem(CadastrarItem parameters, TokenUsuario tokenUsuario)
+			throws AutorizacaoFault {
+		System.out.println("Chamando cadastrar Item");
+		
+		CadastrarItemResponse resposta = new CadastrarItemResponse();
+		resposta.setItem(geraItem());
+		
+		return resposta;
+	}
+	
+	
+	private Item geraItem() {
+		Item item = new Item();
+		item.setCodigo("MEA");
+		item.setNome("MEAN");
+		item.setQuantidade(5);
+		item.setTipo("Livro");
+		
+		return item;
+	}
+
+}
